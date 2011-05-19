240f1194e3a357cdfd5cac05225c21ca3b28cb8e
Ryan Pavlik <rpavlik@iastate.edu>
2011-05-19 13:56:42 -0500

    get the gui working and connecting

--- jconf-display-merger.py
+++ jconf-display-merger.py
@@ -2,9 +2,10 @@
 # Copyright 2011 Iowa State University (Author: Ryan Pavlik <rpavlik@iastate.edu> <abiryan@ryand.net> http://academic.cleardefinition.com/
 #
 import sys
-from PySide.QtCore import QFile
-from PySide.QtGui import QApplication, QMainWindow
-from PySide.QtUiTools import QUiLoader
+import inspect
+from PySide.QtCore import * #QFile
+from PySide.QtGui import * #QApplication, QMainWindow, QAction
+from PySide.QtUiTools import * #QUiLoader
 
 '''Merge display windows.'''
 
@@ -12,23 +13,34 @@ __author__ = "Ryan Pavlik"
 
 __version__ = "0.1"
 
-class MergerWindow(QMainWindow):
-    def __init__(self, parent=None):
-        super(MergerWindow, self).__init__(parent)
-        self.resize(800, 600)  
-        
+class MergerGUI(QObject):
+	def __init__(self, uifn = "display-merger.ui", parent = None):
+		super(MergerGUI, self).__init__(parent)
+		uifile = QFile(uifn)
+		uifile.open(QFile.ReadOnly)
+		loader = QUiLoader()
+		self.window = loader.load(uifile)
+		self.window.show()
+		uifile.close()
+		actions = [ action for action in self.window.findChildren(QAction)
+			if action.objectName() != "separator"
+			and len(action.objectName()) > 0 ]
+		for action in actions:
+			slotname = "on_" + action.objectName()
+			if hasattr(self, slotname):
+				print "Connecting %s" % slotname
+				QObject.connect(action, SIGNAL("triggered()"), getattr(self, slotname))
+			else:
+				print "No method %s found" % slotname
+
+	def on_action_Open(self):
+		print "Open!"
 
 def main():
 	app = QApplication(sys.argv)
 	# Create and show window
-	win = MergerWindow()
-	win.show()
-	uifile = QFile("display-merger.ui")
-	uifile.open(QFile.ReadOnly)
-	loader = QUiLoader()
-	widget = loader.load(uifile)
-	uifile.close()
-	widget.show()
+	gui = MergerGUI()
+
 	# Enter Qt application main loop
 	sys.exit(app.exec_())
 
