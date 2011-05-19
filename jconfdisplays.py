43ceb4947b2526b66aebc028c5d6774dcd15b72f
Ryan Pavlik <rpavlik@iastate.edu>
2011-05-19 15:38:44 -0500

    add bbox class for convenience

--- jconfdisplays.py
+++ jconfdisplays.py
@@ -12,6 +12,28 @@ import sys
 
 ns = "{http://www.vrjuggler.org/jccl/xsd/3.0/configuration}"
 
+class BBox2d(object):
+	def union(bboxes):
+		mins = bboxes[0].origin[:]
+		maxes = [ origin + size for (origin, size) in zip(bboxes[0].origin, bboxes[0].size) ]
+		for bbox in bboxes:
+			for corner in bbox.corners():
+				mins = [ min(prev, current) for (prev, current) in zip(mins, corner) ]
+				maxes = [ max(prev, current) for (prev, current) in zip(maxes, corner) ]
+		return BBox3d(mins, [theMax - theMin for (theMax, theMin) in zip(maxes, mins) ])
+
+	def __init__(self, origin = [0, 0], size = [0, 0]):
+		self.origin = origin
+		self.size = size
+
+	def corners(self):
+		origin = self.origin[:]
+		size = self.size[:]
+		yield origin[:]
+		yield [origin[0] + size[0], origin[1] ]
+		yield [origin[0] + size[0], origin[1] + size[1] ]
+		yield [origin[0], origin[1] + size[1] ]
+
 class SurfaceViewport(object):
 	def __init__(self, elt, window):
 		self.window = window
@@ -20,14 +42,14 @@ class SurfaceViewport(object):
 		self.name = self.elt.get("name")
 		self.origin = self.elt.findall(ns + "origin")
 		self.size = self.elt.findall(ns + "size")
-		self.pixel_origin = [ float(vp.text) * float(dw.text) for (vp, dw) in zip(self.origin, self.window.origin) ]
+		self.pixel_origin = [ float(vp.text) * float(dw.text) + float(dwo.text) for (vp, dw, dwo) in zip(self.origin, self.window.size, self.window.origin) ]
 		self.pixel_size = [ float(vp.text) * float(dw.text) for (vp, dw) in zip(self.size, self.window.size) ]
 		print self.pixel_origin
 		print self.pixel_size
 
 class DisplayWindow(object):
-	def __init__(self, elt, parent):
-		self.parent = parent
+	def __init__(self, elt, jconf):
+		self.jconf = jconf
 		self.elt = elt
 		self.name = elt.get("name")
 		self.origin = elt.findall(ns + "origin")
@@ -39,6 +61,19 @@ class DisplayWindow(object):
 	def addViewport(self, vp):
 		self.elt.append(vp.wrapper)
 		self.surface_viewports.append(vp)
+
+	def merge(self, other):
+		vports = other.surface_viewports[:]
+		originalOrigin = [int(elt.text) for elt in self.origin]
+		newOrigin = [int(elt.text) for elt in self.origin]
+		for coord in range(0, 2):
+			origOther = int(other.origin[coord].text)
+			origSelf = int(self.origin[coord].text)
+			if origOther < origSelf:
+				diff = origSelf - origOther
+				self.origin[coord].text = other.origin[coord].text
+				self.size[coord].text = int(self.size[coord].text) + diff
+
 
 
 class JConf(object):
@@ -55,7 +90,7 @@ class JConf(object):
 				pass
 			elif firstLevel.tag == ns + "elements":
 				self.elements = firstLevel
-				self.display_windows = [ DisplayWindow(window, firstLevel) for window in self.elements.findall(ns + "display_window") ]
+				self.display_windows = [ DisplayWindow(window, self) for window in self.elements.findall(ns + "display_window") ]
 
 			else:
 				continue
