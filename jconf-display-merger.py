2fb7d6866253170dcb5f32f7b26de1d4061805d5
Ryan Pavlik <rpavlik@iastate.edu>
2011-05-20 13:02:54 -0500

    Apply BSL license

--- jconf-display-merger.py
+++ jconf-display-merger.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
-# Copyright 2011 Iowa State University (Author: Ryan Pavlik <rpavlik@iastate.edu> <abiryan@ryand.net> http://academic.cleardefinition.com/
-#
+# Author: Ryan Pavlik <rpavlik@iastate.edu> <abiryan@ryand.net> http://academic.cleardefinition.com/
+
+#          Copyright Iowa State University 2011.
+# Distributed under the Boost Software License, Version 1.0.
+#    (See accompanying file LICENSE_1_0.txt or copy at
+#          http://www.boost.org/LICENSE_1_0.txt)
+
 import sys
 import os
 import jconfdisplays
